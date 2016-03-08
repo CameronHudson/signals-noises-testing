a72f573c5fb1b9c24312e518df36b24fefb31f31
CameronHudson <CameronHudson@users.noreply.github.com>
2016-03-08 14:17:27 -0500

    Added the folder that I said in the previous commit...

---
+++ Determining the Transfer Function/GenerateData.m
@@ -0,0 +1,52 @@
+blackBox
+set(blackBox,'visible','off');
+
+Ts = 0.002; %SampleTime
+fileName = 'chirpFunc';
+
+
+% Find handle to hidden figure
+temp = get(0,'showHiddenHandles');
+set(0,'showHiddenHandles','on');
+hfig = gcf;
+% Get the handles structure
+handles = guidata(hfig);
+
+%================Start Editing=============================================
+
+% This will let you pick the Field radio button
+set(handles.radioField, 'Value', 1);
+% This changes the equation in the Field textbox
+set(handles.input, 'String', 'exp(-t)*2*sin(t^2)' );
+blackBox('input_Callback',handles.input,[],handles);
+
+
+% This changes the start time
+set(handles.axisStart, 'String', '0');
+% This changes the end time
+set(handles.axisEnd, 'String', '100');
+% This changes the step size
+set(handles.stepSize, 'String', Ts);
+% This changes the refine output
+set(handles.refineOutput, 'String', '1');
+
+% Use the run button twice since the black box sucks a lot
+blackBox('run_Callback',handles.run,[],handles);
+blackBox('run_Callback',handles.run,[],handles);
+
+
+% Change the save file name
+set(handles.saveFile, 'String', fileName );
+% Save
+blackBox('save_Callback',handles.save,[],handles);
+
+% % For generating sine waves at varying amplitudes instead of the chirp
+% k = 10;
+% name = sprintf('sin(%d*t)',k);
+% set(handles.input, 'String', name );
+% blackBox('input_Callback',handles.input,[],handles);
+
+%=======================End Editing======================================
+set(0,'showHiddenHandles',temp);
+close(blackBox);
+
---
+++ Determining the Transfer Function/ProcessAndFindOrder.m
@@ -0,0 +1,32 @@
+%Preferences - Must be set identically to the GenerateData file
+Ts = 0.002; %SampleTime
+fileName = 'chirpFunc';
+
+%Convert Data into a usable format
+dataSet = load(fileName);
+Y = dataSet.(fileName).output.signal;
+U = dataSet.(fileName).input.signal;
+time = dataSet.(fileName).input.time;
+
+
+%Often, the output produced is 2-5 elements larger than the input. Don't
+%know why. This cleans it by trimming the output
+IDim = size(U);
+ODim = size(Y);
+Overage = ODim(1)-IDim(1);
+
+CleanedY = Y;
+
+for i = 1:Overage
+    CleanedY(IDim(1)+1) = [];
+end
+
+IOData = iddata(CleanedY,U,Ts);
+
+SSmodelNoise = n4sid(IOData,1:10); %Includes estimate of K noise matrix
+tfNoise = ss2tf(SSmodelNoise.a,SSmodelNoise.b,SSmodelNoise.c,SSmodelNoise.d)
+SSmodelClean = n4sid(IOData,1:10,'DisturbanceModel','none'); %Omits K noise matrix
+tfClean = ss2tf(SSmodelClean.a,SSmodelClean.b,SSmodelClean.c,SSmodelClean.d)
+
+
+
