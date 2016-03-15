92092330a1c0b8b3a723886ee0894009ad1168ac
CameronHudson <CameronHudson@users.noreply.github.com>
2016-03-15 14:20:32 -0400

    code for processing impulse data via tfest

--- Determining Freq Response/FFTProcess.m
+++ Determining Freq Response/FFTProcess.m
@@ -9,8 +9,9 @@ Signal(length(In):length(Signal)-1) = [];
 Time = StepOut.input.time;
 TransformedInput = fft(In/Ts);
 TransformedNoise = fft(Signal/Ts);
+something = (TransformedNoise./TransformedInput);
 
-booty = abs((TransformedNoise./TransformedInput)*Ts);
+booty = abs(something*Ts);
 N = length(Time);
 frequency = [0:N-1]'/(N*Ts);
 
@@ -20,9 +21,20 @@ xvals = loglog(frequency,booty);
 figure(2);
 bode(ans);
 
-y=fft(booty);
-p=unwrap(angle(y));
+p=unwrap(angle(something)*180/pi);
 
 
 figure(3)
 semilogx(frequency,p)
+
+%% Process & TFest
+
+TFInput = TransformedInput(1:4917100,1)*Ts;
+TFOutput = TransformedNoise(1:4917100,1)*Ts;
+TFFreq = frequency(1:4917100,1);
+
+TFdata = iddata(TFOutput, TFInput, Ts,'Frequency',TFFreq);
+
+sys = tfest(TFdata,6);
+
+
